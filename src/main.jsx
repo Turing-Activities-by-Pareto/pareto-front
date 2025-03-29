8a3c91b2b3fc3bab3df2ca21ce60ea91f522e175
kenanqafarov <qafarovkenan06@gmail.com>
2025-03-29 17:58:16 +0400

    routes_and_etc_added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./pages/App";
+
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import AppRoutes from "./routes/AppRoutues";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Header />
+    <AppRoutes />
+    <Footer />
   </StrictMode>
 );
