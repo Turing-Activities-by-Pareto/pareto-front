dc33db4ff80509d09c5fcf26ce12ec89ee21406a
Fatya <fatimaanovruzova@gmail.com>
2025-03-30 17:01:31 +0400

    addEvent

---
+++ src/pages/AddEvents.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../assets/styles/AddEvents/AddEvents.css"
+const AddEvents = () => {
+  return (
+    <>
+    <div className="head">
+      <div className="head_content">
+      <div className="left_part">
+            <h2>Tədbir əlavə et</h2>
+            <p>Birlikdə daha canlı və ilhamverici bir atmosfer quraq!</p>
+        </div>
+        <div className="right_part">
+          <img src="" alt="icon" />
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default AddEvents;
--- src/routes/AppRoutues.jsx
+++ src/routes/AppRoutues.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 import Home from "../pages/Home";
+import AddEvents from "../pages/AddEvents";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/add_events" element={<AddEvents />} />
       </Routes>
     </BrowserRouter>
   );
